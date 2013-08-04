c5e16e854144fe6a5815bbc8a08d5a4799f52fb8
Michael DiStaula <distaula@gmail.com>
2013-08-04 13:14:47 -0400

    Added views

--- Controllers/LightsController.cs
+++ Controllers/LightsController.cs
@@ -1,23 +1,124 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
-using Q42.HueApi.Interfaces;
+using Q42.HueApi;
+using BootstrapDiStaula.Models;
 
 namespace BootstrapDiStaula.Controllers
 {
-	public class LightsController : AsyncController
+	public class LightsController : Controller
 	{
-		private readonly IEnumerable<string> _lightList = new List<string> { "1", "2", "3", "4", "6" };
+		private readonly ILightRepository lightRepository;
 
-		private IHueClient _hueClient;
-
-		public LightsController(IHueClient hueClient)
+		// If you are using Dependency Injection, you can delete the following constructor
+		public LightsController()
+			: this(new LightRepository())
 		{
-			_hueClient = hueClient;
 		}
 
-		public ActionResult Index()
+		public LightsController(ILightRepository lightRepository)
+		{
+			this.lightRepository = lightRepository;
+		}
+
+		//
+		// GET: /Default1/
+
+		public ViewResult Index()
+		{
+			return View(lightRepository.All);
+		}
+
+		//
+		// GET: /Default1/Details/5
+
+		public ViewResult Details(string id)
+		{
+			return View(lightRepository.Find(id));
+		}
+
+		//
+		// GET: /Default1/Create
+
+		public ActionResult Create()
 		{
 			return View();
 		}
+
+		//
+		// POST: /Default1/Create
+
+		[HttpPost]
+		public ActionResult Create(Light light)
+		{
+			if (ModelState.IsValid)
+			{
+				lightRepository.InsertOrUpdate(light);
+				lightRepository.Save();
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+		//
+		// GET: /Default1/Edit/5
+
+		public ActionResult Edit(string id)
+		{
+			return View(lightRepository.Find(id));
+		}
+
+		//
+		// POST: /Default1/Edit/5
+
+		[HttpPost]
+		public ActionResult Edit(Light light)
+		{
+			if (ModelState.IsValid)
+			{
+				lightRepository.InsertOrUpdate(light);
+				lightRepository.Save();
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+		//
+		// GET: /Default1/Delete/5
+
+		public ActionResult Delete(string id)
+		{
+			return View(lightRepository.Find(id));
+		}
+
+		//
+		// POST: /Default1/Delete/5
+
+		[HttpPost, ActionName("Delete")]
+		public ActionResult DeleteConfirmed(string id)
+		{
+			lightRepository.Delete(id);
+			lightRepository.Save();
+
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				lightRepository.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 	}
 }
+
